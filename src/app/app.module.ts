a7e008057f6713a5e04d10cbeebb36e71306fdf7
Evaine Platt <evainecplatt@gmail.com>
2019-02-27 21:19:10 -0500

    added signup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    LandingComponent
   ],
   imports: [
     FormsModule,
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { allocExpando } from '@angular/core/src/render3/instructions';
+import { NgIf } from '@angular/common';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignupComponent implements OnInit {
+
+  signupForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+
+    this.signupForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      streetName: new FormControl(),
+      streetNum: new FormControl(),
+      city: new FormControl(),
+      country: new FormControl(),
+      zipcode: new FormControl(),
+      email: new FormControl(),
+      password: new FormControl()
+    });
+      }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log('Form Valid!');
+      console.log(this.signupForm.value);
+    } else {
+      console.log('INVALID FORM REEEEEEEE')
+    }
   }
 
+  signup() {
+    console.log(this.signupForm);
+  }
 }
