99d89c10685a52821f46807aa8c2eeba3eb65ea6
Evaine Platt <evainecplatt@gmail.com>
2019-03-03 16:16:14 -0500

    login.html merge

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { allocExpando } from '@angular/core/src/render3/instructions';
+import { NgIf } from '@angular/common';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { LoginService } from '../../services/login-service.service';
 import { MarketPlaceUserDataService } from '../../services/market-place-user-data.service';
-
+import { CookieService } from 'ngx-cookie-service';
 import { MarketPlaceUser } from '../../models/market-place-user';
+import { Router } from '@angular/router';
+
 
 @Component({
     selector: 'app-login',
@@ -14,44 +20,52 @@ import { MarketPlaceUser } from '../../models/market-place-user';
 })
 export class LoginComponent implements OnInit {
 
-    loginForm: FormGroup;
+    @Output()
+    showSignEvent = new EventEmitter<object>();
+    showSignIn = true;
+    private loginForm: FormGroup;
 
     constructor(
-        private router: Router,
         private loginService: LoginService,
-        private marketPlaceUserDataService: MarketPlaceUserDataService) { }
+        private marketPlaceUserDataService: MarketPlaceUserDataService,
+        private router: Router,
+        private cookie: CookieService) { }
 
     ngOnInit() {
+        this.cookie.deleteAll();
         this.loginForm = this.createLoginForm();
     }
 
     onSubmit() {
         const form = this.loginForm;
         if (form.valid) {
-            /**
-             * Fetch user from the database
-             */
-            const userData = form.value;
-            console.log(form.value);
-            this.loginService.login(userData).subscribe(
+            this.loginService.login(form.value).subscribe(
                 (response) => {
                     console.log(response);
+                    const marketPlaceUser = response;
+                    this.setMarketPlaceUser(marketPlaceUser);
+                    this.showSignIn = false;
+                    this.showSignEvent.emit({ showSignIn: this.showSignIn, firstname: response.firstname });
+                    this.cookie.set('firstname', response.firstname);
+                    this.cookie.set('mpuid', response.id);
+                    this.router.navigateByUrl('marketPage');
                 });
         } else {
-            console.log('Invalid Form!');
+            alert('Invalid form!');
         }
     }
 
     private createLoginForm(): FormGroup {
         return new FormGroup({
             email: new FormControl(),
-            password: new FormControl(),
-            rememberMe: new FormControl()
+            password: new FormControl()
         });
     }
 
     private setMarketPlaceUser(marketPlaceUser: MarketPlaceUser) {
+
         this.marketPlaceUserDataService.changeMarketPlaceUser(marketPlaceUser);
+
     }
 
 }
